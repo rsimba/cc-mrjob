f3431cdea5d4fbcfad39a624e2038eff6b02fae2
Alex Ianus <hire@alexianus.com>
2015-03-21 15:16:05 -0400

    Move mrcc into job file and improve performance

--- socialspark.py
+++ socialspark.py
@@ -1,7 +1,42 @@
 import re
 from urlparse import urlparse
 from collections import Counter
-from mrcc import CCJob
+import gzip
+#
+import boto
+import warc
+#
+from boto.s3.key import Key
+from gzipstream import GzipStreamFile
+from mrjob.job import MRJob
+
+class CCJob(MRJob):
+  def process_record(self, record):
+    """
+    Override process_record with your mapper
+    """
+    raise NotImplementedError('Process record needs to be customized')
+
+  def mapper(self, _, line):
+    f = None
+    ## If we're on EC2 or running on a Hadoop cluster, pull files via S3
+    if self.options.runner in ['emr', 'hadoop']:
+      # Connect to Amazon S3 using anonymous credentials
+      conn = boto.connect_s3(anon=True)
+      pds = conn.get_bucket('aws-publicdatasets')
+      # Start a connection to one of the WARC files
+      k = Key(pds, line)
+      f = warc.WARCFile(fileobj=GzipStreamFile(k))
+    ## If we're local, use files on the local file system
+    else:
+      print 'Loading local file {}'.format(line)
+      f = warc.WARCFile(fileobj=gzip.open(line))
+    ###
+    for i, record in enumerate(f):
+      for key, value in self.process_record(record):
+        yield key, value
+      self.increment_counter('commoncrawl', 'processed_records', 1)
+
 TWITTER_PATTERN = re.compile("twitter\.com\/[a-zA-Z0-9_]{1,15}\/?\Z")
 YOUTUBE_PATTERN = re.compile("youtube\.com\/user\/[a-zA-Z0-9]{1,20}\/?\Z")
 INSTAGRAM_PATTERN = re.compile("instagram\.com\/[a-zA-Z0-9_.]{1,30}\/?\Z")
@@ -10,9 +45,6 @@ class SocialSparkRegexJob(CCJob):
   def process_record(self, record):
     if record['Content-Type'] == 'application/http; msgtype=response':
       url = record["WARC-Target-URI"]
-      payload = record.payload.read()
-      headers, body = payload.split('\r\n\r\n', 1)
-      domain = urlparse(url).netloc
 
       found = False
       if INSTAGRAM_PATTERN.findall(url):
@@ -35,20 +67,17 @@ class SocialSparkRegexJob(CCJob):
       '''
 
       if found:
-        print url
+        payload = record.payload.read()
+        headers, body = payload.split('\r\n\r\n', 1)
+        domain = urlparse(url).netloc
         yield domain, tuple([{url:body}])
         self.increment_counter('commoncrawl', 'processed_pages', 1)
 
   def combiner(self, key, values):
-    #yield key, next(values)
-    #yield key, values
-    for val in values:
-      yield key, val
+    yield key, next(values)
 
   def reducer(self, key, values):
-    #yield key, next(values)
-    for val in values:
-      yield key, val
+    yield key, next(values)
 
 if __name__ == '__main__':
   SocialSparkRegexJob.run()
